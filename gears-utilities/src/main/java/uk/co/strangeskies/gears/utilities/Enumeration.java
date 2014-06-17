5845ed6de7531a758fc8ca62300c7525c6f7d306
eliasvasylenko <eliasvasylenko@gmail.com>
2014-06-17 08:49:05 +0100

    New Enumeration class (for cases enum can't handle, such as enumerations with generic type parameters...)

---
+++ gears-utilities/src/main/java/uk/co/strangeskies/gears/utilities/Enumeration.java
@@ -0,0 +1,84 @@
+package uk.co.strangeskies.gears.utilities;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Enumeration<S extends Enumeration<S>> implements Self<S> {
+	private static class EnumerationType<T extends Enumeration<T>> {
+		private final List<T> instances;
+
+		public EnumerationType() {
+			instances = new ArrayList<>();
+		}
+
+		public List<T> getInstances() {
+			return instances;
+		}
+	}
+
+	private static final Map<Class<?>, EnumerationType<?>> ENUM_TYPES = new HashMap<>();
+
+	private final String name;
+	private final int ordinal;
+
+	protected Enumeration(String name) {
+		if (getEnumerationConstants((Class<T>) getClass()).stream().anyMatch(
+				e -> e.name().equals(name))) throw new IllegalArgumentException();
+
+		this.name = name;
+		ordinal = addInstance(getThis());
+	}
+
+	public String name() {
+		return name;
+	}
+
+	public int ordinal() {
+		return ordinal;
+	}
+
+	@Override
+	public String toString() {
+		return name();
+	};
+
+	@Override
+	public S copy() {
+		return getThis();
+	}
+
+	@SuppressWarnings("unchecked")
+	private static <T extends Enumeration<T>> int addInstance(T instance) {
+		List<T> enumerationConstants = getEnumerationType(
+				((Class<T>) instance.getClass())).getInstances();
+
+		int ordinal = enumerationConstants.size();
+		enumerationConstants.add(instance);
+
+		return ordinal;
+	}
+
+	private static <T extends Enumeration<T>> EnumerationType<T> getEnumerationType(
+			Class<T> type) {
+		@SuppressWarnings("unchecked")
+		EnumerationType<T> enumType = (EnumerationType<T>) ENUM_TYPES.get(type);
+		if (enumType == null)
+			ENUM_TYPES.put(type, new EnumerationType<T>());
+		return enumType;
+	}
+
+	public static <T extends Enumeration<T>> List<T> getEnumerationConstants(
+			Class<T> enumerationClass) {
+		return Collections.unmodifiableList(getEnumerationType(enumerationClass)
+				.getInstances());
+	}
+
+	public static <T extends Enumeration<T>> T valueOf(Class<T> enumerationClass,
+			String name) {
+		return getEnumerationConstants(enumerationClass).stream()
+				.filter(e -> e.name().equals(name)).findAny().get();
+	}
+}
